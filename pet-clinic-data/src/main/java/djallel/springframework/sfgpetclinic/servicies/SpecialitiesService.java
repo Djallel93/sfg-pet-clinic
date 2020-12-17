d631832db73db365c9e8dcbd2bf44d764370c09a
Djallel93 <bouakaz.djallel@gmail.com>
2020-12-17 09:51:19 +0100

    Added Specialties Map Impl, close #31.

---
+++ pet-clinic-data/src/main/java/djallel/springframework/sfgpetclinic/servicies/SpecialitiesService.java
@@ -0,0 +1,8 @@
+package djallel.springframework.sfgpetclinic.servicies;
+
+import djallel.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+
+
+}
