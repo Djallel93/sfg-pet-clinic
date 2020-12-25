9cf8a0f912f2ce0b48085fab3aae92689a085785
Djallel93 <bouakaz.djallel@gmail.com>
2020-12-25 09:35:05 +0100

    Added CRUD tests for OwnerMapService. Closes #49

--- pet-clinic-data/src/test/java/djallel/springframework/sfgpetclinic/servicies/map/OwnerMapServiceTest.java
+++ pet-clinic-data/src/test/java/djallel/springframework/sfgpetclinic/servicies/map/OwnerMapServiceTest.java
@@ -7,16 +7,18 @@ import org.junit.jupiter.api.Test;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class OwnerMapServiceTest {
 
     OwnerMapService ownerMapService;
     final Long ownerId = 1L;
+    final String lastName = " Smith";
 
     @BeforeEach
     void setUp() {
         ownerMapService = new OwnerMapService(new PetTypeMapService(), new PetMapService());
-        ownerMapService.save(Owner.builder().id(ownerId).build());
+        ownerMapService.save(Owner.builder().id(ownerId).lastName(lastName).build());
     }
 
     @Test
@@ -37,25 +39,39 @@ class OwnerMapServiceTest {
     void saveExistingId() {
 
         Long id = 2l;
-
         Owner owner2 = Owner.builder().id(id).build();
-
-        Owner savedOwner= ownerMapService.save(owner2);
-
+        Owner savedOwner = ownerMapService.save(owner2);
         assertEquals(id, savedOwner.getId());
+    }
+
+    @Test
+    void saveNoId() {
+        Owner savedOwner = ownerMapService.save(Owner.builder().build());
+        assertNotNull(savedOwner);
+        assertNotNull(savedOwner.getId());
 
     }
 
     @Test
     void delete() {
 
+        ownerMapService.delete(ownerMapService.findByID(ownerId));
+
+        assertEquals(0, ownerMapService.findAll().size());
+
     }
 
     @Test
     void deleteById() {
+        ownerMapService.deleteById(ownerId);
+        assertEquals(0, ownerMapService.findAll().size());
     }
 
     @Test
     void findByLastName() {
+        Owner smith = ownerMapService.findByLastName(lastName);
+        assertNotNull(smith);
+        assertEquals(ownerId, smith.getId());
+
     }
 }
